b7cf3c24b49f19ffc1e18125e69f79cc6898be9b
Mark Weitzel <markweitzel@microsoft.com>
2023-03-23 21:41:38 -0400

    still finding nits. sigh.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://apipracticepatterns.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/APIPracticePatterns/',
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
 
