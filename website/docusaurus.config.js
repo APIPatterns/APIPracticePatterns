1419d92e40637374df1521cdade81de2fe859fd6
Mark Weitzel <markweitzel@microsoft.com>
2023-03-23 21:27:39 -0400

    setting up gh pages

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,11 +11,12 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://APIPatterns/APIPracticePatterns.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  trailingSlash: false,
+  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'APIPatterns', // Usually your GitHub org/user name.
